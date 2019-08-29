15b64321d9bb22b425edfdcf3ba5dac78f5b6553
Nick Sjostrom <nsjostrom@dyn.com>
2019-08-29 10:44:55 -0400

    post tag to repo

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,9 @@
 # get latest tag
 t=$(git describe --tags `git rev-list --tags --max-count=1`)
 
+# get current commit hash for tag
+commit=$(git rev-parse HEAD)
+
 # if there are none, start tags at 0.0.0
 if [ -z "$t" ]
 then
@@ -21,7 +24,25 @@ case "$log" in
 esac
 
 echo $new
-echo $TEST_SECRET
+echo $GITHUB_TOKEN
 
-# git tag $new
-# git push origin $new
+dt=$(date '+%Y-%m-%dT%H:%M:%SZ')
+repo=$(basename -s .git `git config --get remote.origin.url`)
+
+echo "pushing tag $new to repo $REPO_OWNER/$repo"
+
+curl -0 -v -X POST https://api.github.com/repos/$REPO_OWNER/$repo/git/tags \
+-H "Authorization: token $ACCESS_TOKEN" \
+-d @- << EOF
+
+{
+  "tag": "$new",
+  "message": "Bump version to $new",
+  "object": "$commit",
+  "type": "commit",
+  "tagger": {
+    "name": "anothrNick/github-tag-action",
+    "date": "$dt"
+  }
+}
+EOF
