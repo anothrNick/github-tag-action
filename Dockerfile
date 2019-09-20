4c66efb250efaea0f088e6d30d5824522147bcfe
Reece Como <reece.como@gmail.com>
2019-09-20 13:34:56 +0800

    Add dry run and hotfix

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,12 @@
 FROM prontotools/alpine-git-curl
-LABEL "com.github.actions.name"="Github Tag Bump"
-LABEL "com.github.actions.description"="Bump and push git tag on merge"
+LABEL "com.github.actions.name"="Auto-Tag Release"
+LABEL "com.github.actions.description"="Automatically bump semantic version tags"
 LABEL "com.github.actions.icon"="git-merge"
 LABEL "com.github.actions.color"="purple"
 
 LABEL "repository"="https://github.com/reececomo/auto-tag-release"
-LABEL "homepage"="https://github.com/reececomo/auto-tag-release" 
-LABEL "maintainer"="Nick Sjostrom"
+LABEL "homepage"="https://github.com/reececomo/auto-tag-release"
+LABEL "maintainer"="Reece Como"
 
 COPY ./contrib/semver ./contrib/semver
 RUN install ./contrib/semver /usr/local/bin
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Config
-default_bump=$(DEFAULT_BUMP:-minor)
+default_semvar_bump=$(DEFAULT_BUMP:-minor)
+dry_run_mode=$(DRY_RUN:-false)
 
 # get latest tag
 tag=$(git describe --tags `git rev-list --tags --max-count=1`)
@@ -11,7 +12,7 @@ t_commit=$(git rev-list -n 1 $tagag)
 commit=$(git rev-parse HEAD)
 
 if [ "$tag_commit" == "$commit" ]; then
-    echo "No new commits since previous tag."
+    echo "No new commits since previous tag. Skipping..."
     exit 0
 fi
 
@@ -25,11 +26,14 @@ else
 fi
 
 # get commit logs and determine home to bump the version
-# supports #major, #minor, #patch (anything else will be 'minor')
+# supports #major, #minor, #patch (anything else will be '$default_semvar_bump')
 case "$log" in
     *#major* ) new=$(semver bump major $tag);;
+    *#minor* ) new=$(semver bump minor $tag);;
     *#patch* ) new=$(semver bump patch $tag);;
-    * ) new=$(semver bump minor $tag);;
+    *hot-fix* ) new=$(semver bump patch $tag);;
+    *hotfix* ) new=$(semver bump patch $tag);;
+    * ) new=$(semver bump `$default_semvar_bump` $tag);;
 esac
 
 echo $new
@@ -37,6 +41,11 @@ echo $new
 dt=$(date '+%Y-%m-%dT%H:%M:%SZ')
 remote=$(git config --get remote.origin.url)
 repo=$(basename $remote .git)
+
+if [ "$dry_run_mode" = true ] ; then
+    echo "[DRY-RUN] New version tag is: $new"
+    exit 0
+fi
 
 echo "$dt: **pushing tag $new to repo $REPO_OWNER/$repo"
 
