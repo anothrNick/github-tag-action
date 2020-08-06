56686c5b7f581713e8a9b1ba8df42585ddd30bcc
Adam Oliver Zsigmond <zsedem@gmail.com>
2020-08-06 16:06:48 +0200

    use actual regexp match instead of globs for tag matcher
    The former implementation matched on versions like 25.0.0-RC1
    or 1.3.4-M2

--- entrypoint.sh
+++ entrypoint.sh
@@ -30,8 +30,8 @@ git fetch --tags
 
 # get latest tag that looks like a semver (with or without v)
 case "$tag_context" in
-    *repo*) tag=$(git for-each-ref --sort=-v:refname --count=1 --format '%(refname)' refs/tags/[0-9]*.[0-9]*.[0-9]* refs/tags/v[0-9]*.[0-9]*.[0-9]* | cut -d / -f 3-);;
-    *branch*) tag=$(git describe --tags --match "*[v0-9].*[0-9\.]" --abbrev=0);;
+    *repo*) tag=$(git for-each-ref --sort=-v:refname --format '%(refname)' | cut -d / -f 3- | grep -E '^v?[0-9]+.[0-9]+.[0-9]+$' | head -n1);;
+    *branch*) tag=$(git tag --list --merged HEAD --sort=-committerdate | grep -E '^v?[0-9]+.[0-9]+.[0-9]+$' | head -n1);;
     * ) echo "Unrecognised context"; exit 1;;
 esac
 
