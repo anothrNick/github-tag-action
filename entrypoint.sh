5659a7e67b46e9ccd891235b1f1330d520baabc9
Kevin Gentile <kevin.gentile@rockwellautomation.com>
2022-02-11 12:26:00 -0500

    fix quoted input compatability with semver newlines

--- entrypoint.sh
+++ entrypoint.sh
@@ -54,14 +54,14 @@ case "$tag_context" in
         tag="$(semver $taglist | tail -n 1)"
 
         pre_taglist="$(git for-each-ref --sort=-v:refname --format '%(refname:lstrip=2)' | grep -E "$preTagFmt")"
-        pre_tag="$(semver "$pre_taglist" | tail -n 1)"
+        pre_tag="$(semver $pre_taglist | tail -n 1)"
         ;;
     *branch*) 
         taglist="$(git tag --list --merged HEAD --sort=-v:refname | grep -E "$tagFmt")"
         tag="$(semver $taglist | tail -n 1)"
 
         pre_taglist="$(git tag --list --merged HEAD --sort=-v:refname | grep -E "$preTagFmt")"
-        pre_tag=$(semver "$pre_taglist" | tail -n 1)
+        pre_tag=$(semver $pre_taglist | tail -n 1)
         ;;
     * ) echo "Unrecognised context"; exit 1;;
 esac
