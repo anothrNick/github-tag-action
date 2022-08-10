153314eeb8cb94ab40c92bd5e3306fd6e60dab2f
Nathaniel Young <nathanielyoung95070@gmail.com>
2022-08-09 17:42:17 -0700

    fixes tag if there are no existing tags

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,8 @@ LABEL "repository"="https://github.com/anothrNick/github-tag-action"
 LABEL "homepage"="https://github.com/anothrNick/github-tag-action"
 LABEL "maintainer"="Nick Sjostrom"
 
-COPY entrypoint.sh /entrypoint.sh
-
 RUN apk update && apk add bash git curl jq && npm install -g semver
+
+COPY entrypoint.sh /entrypoint.sh
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -50,18 +50,26 @@ git fetch --tags
 tagFmt="^v?[0-9]+\.[0-9]+\.[0-9]+$" 
 preTagFmt="^v?[0-9]+\.[0-9]+\.[0-9]+(-$suffix\.[0-9]+)?$" 
 
+tag=""
+
 # get latest tag that looks like a semver (with or without v)
 case "$tag_context" in
     *repo*) 
         taglist="$(git for-each-ref --sort=-v:refname --format '%(refname:lstrip=2)' | grep -E "$tagFmt")"
-        tag="$(semver $taglist | tail -n 1)"
+        if [ ! -z "$taglist" ]
+        then
+            tag="$(semver $taglist | tail -n 1)"
+        fi
 
         pre_taglist="$(git for-each-ref --sort=-v:refname --format '%(refname:lstrip=2)' | grep -E "$preTagFmt")"
         pre_tag="$(semver "$pre_taglist" | tail -n 1)"
         ;;
     *branch*) 
         taglist="$(git tag --list --merged HEAD --sort=-v:refname | grep -E "$tagFmt")"
-        tag="$(semver $taglist | tail -n 1)"
+        if [ ! -z "$taglist" ]
+        then
+            tag="$(semver $taglist | tail -n 1)"
+        fi
 
         pre_taglist="$(git tag --list --merged HEAD --sort=-v:refname | grep -E "$preTagFmt")"
         pre_tag=$(semver "$pre_taglist" | tail -n 1)
