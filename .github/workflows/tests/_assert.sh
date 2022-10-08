2f4c3b24f35bf97af18f1e735608a6a32fa00aba
Łukasz Sypniewski <l.sypniewski@gmail.com>
2022-10-08 12:26:38 +0200

    All tests are ran in a single job

--- .github/workflows/tests/_assert.sh
+++ .github/workflows/tests/_assert.sh
@@ -5,7 +5,9 @@ CORRECT_TAG=$2
 
 if [[ $OUTPUT_NEWTAG == "${CORRECT_TAG}" ]]; then
     echo "The tag was created correctly" >>$GITHUB_STEP_SUMMARY
+    rm -rf test-repo
 else
     echo "The tag was not created correctly, expected $CORRECT_TAG got $OUTPUT_NEWTAG" >>$GITHUB_STEP_SUMMARY
+    rm -rf test-repo
     exit 1
 fi
