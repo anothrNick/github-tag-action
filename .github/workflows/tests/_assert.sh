a58ad8e41e3b45f7a2bf95eba84cbd93d8294617
Łukasz Sypniewski <l.sypniewski@gmail.com>
2022-10-08 12:33:10 +0200

    Fix shellcheck warnings

--- .github/workflows/tests/_assert.sh
+++ .github/workflows/tests/_assert.sh
@@ -4,10 +4,10 @@ OUTPUT_NEWTAG=$1
 CORRECT_TAG=$2
 
 if [[ $OUTPUT_NEWTAG == "${CORRECT_TAG}" ]]; then
-    echo "The tag was created correctly" >>$GITHUB_STEP_SUMMARY
+    echo "The tag was created correctly" >>"$GITHUB_STEP_SUMMARY"
     rm -rf test-repo
 else
-    echo "The tag was not created correctly, expected $CORRECT_TAG got $OUTPUT_NEWTAG" >>$GITHUB_STEP_SUMMARY
+    echo "The tag was not created correctly, expected $CORRECT_TAG got $OUTPUT_NEWTAG" >>"$GITHUB_STEP_SUMMARY"
     rm -rf test-repo
     exit 1
 fi
