7995bc250268dbc8a93d6d1b65ef0f0530126dfb
Łukasz Sypniewski <l.sypniewski@gmail.com>
2023-01-11 00:39:11 +0100

    Unify style in 'if' condition

--- .github/workflows/tests/_assert.sh
+++ .github/workflows/tests/_assert.sh
@@ -3,11 +3,11 @@
 OUTPUT_NEWTAG=$1
 CORRECT_TAG=$2
 
-if [[ $OUTPUT_NEWTAG == "${CORRECT_TAG}" ]]; then
+if [[ "${OUTPUT_NEWTAG}" == "${CORRECT_TAG}" ]]; then
     echo "The tag was created correctly" >>"$GITHUB_STEP_SUMMARY"
     rm -rf test-repo
 else
     echo "The tag was not created correctly, expected $CORRECT_TAG got $OUTPUT_NEWTAG" >>"$GITHUB_STEP_SUMMARY"
     rm -rf test-repo
-    exit 123
+    exit 1
 fi
