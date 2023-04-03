441a86c802f9e9223e146cc13f8c8bc9dda6b671
beholdenkey <justthered63@gmail.com>
2023-04-03 16:56:31 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,22 +27,22 @@ cd "${GITHUB_WORKSPACE}/${source_dir}" || exit 1
 # Display configurations
 echo "*** CONFIGURATION ***"
 config_vars=(
-    "DEFAULT_BUMP" "DEFAULT_BRANCH" "WITH_V" "RELEASE_BRANCHES"
-    "CUSTOM_TAG" "SOURCE" "DRY_RUN" "INITIAL_VERSION" "TAG_CONTEXT"
-    "PRERELEASE" "PRERELEASE_SUFFIX" "VERBOSE" "MAJOR_STRING_TOKEN"
-    "MINOR_STRING_TOKEN" "PATCH_STRING_TOKEN" "NONE_STRING_TOKEN"
-    "BRANCH_HISTORY"
+	"DEFAULT_BUMP" "DEFAULT_BRANCH" "WITH_V" "RELEASE_BRANCHES"
+	"CUSTOM_TAG" "SOURCE" "DRY_RUN" "INITIAL_VERSION" "TAG_CONTEXT"
+	"PRERELEASE" "PRERELEASE_SUFFIX" "VERBOSE" "MAJOR_STRING_TOKEN"
+	"MINOR_STRING_TOKEN" "PATCH_STRING_TOKEN" "NONE_STRING_TOKEN"
+	"BRANCH_HISTORY"
 )
 for var in "${config_vars[@]}"; do
-    echo -e "\t${var}: ${!var}"
+	echo -e "\t${var}: ${!var}"
 done
 
 if $verbose; then
-    set -x
+	set -x
 fi
 
 setOutput() {
-    echo "${1}=${2}" >>"${GITHUB_OUTPUT}"
+	echo "${1}=${2}" >>"${GITHUB_OUTPUT}"
 }
 
 current_branch=$(git rev-parse --abbrev-ref HEAD)
@@ -51,9 +51,9 @@ current_branch=$(git rev-parse --abbrev-ref HEAD)
 pre_release="$prerelease"
 IFS=',' read -ra branches <<<"$release_branches"
 for branch in "${branches[@]}"; do
-    if [[ $current_branch == "$branch" ]] || [[ $current_branch =~ $branch ]]; then
-        pre_release="false"
-    fi
+	if [[ $current_branch == "$branch" ]] || [[ $current_branch =~ $branch ]]; then
+		pre_release="false"
+	fi
 done
 echo "pre_release = $pre_release"
 
@@ -65,45 +65,44 @@ pre_tag_fmt="^v?[0-9]+\.[0-9]+\.[0-9]+(-$prerelease_suffix\.[0-9]+)$"
 
 # Get latest tags
 get_latest_tag() {
-    local pattern=$1
-    local latest_tag
+	local pattern=$1
+	local latest_tag
 
-    if [ -z "$pattern" ]; then
-        echo "Error: No pattern provided."
-        exit 1
-    fi
+	if [ -z "$pattern" ]; then
+		echo "Error: No pattern provided."
+		exit 1
+	fi
 
-    case "$tag_context" in
-    *repo*)
-        latest_tag=$(git for-each-ref --sort=-v:refname --format '%(refname:lstrip=2)' 2>/dev/null | grep -E "$pattern" | head -n 1)
-        ;;
-    *branch*)
-        latest_tag=$(git tag --list --merged HEAD --sort=-v:refname 2>/dev/null | grep -E "$pattern" | head -n 1)
-        ;;
-    *)
-        echo "Error: Unrecognised context."
-        exit 1
-        ;;
-    esac
+	case "$tag_context" in
+	*repo*)
+		latest_tag=$(git for-each-ref --sort=-v:refname --format '%(refname:lstrip=2)' 2>/dev/null | grep -E "$pattern" | head -n 1)
+		;;
+	*branch*)
+		latest_tag=$(git tag --list --merged HEAD --sort=-v:refname 2>/dev/null | grep -E "$pattern" | head -n 1)
+		;;
+	*)
+		echo "Error: Unrecognised context."
+		exit 1
+		;;
+	esac
 
-    if [ -z "$latest_tag" ]; then
-        echo "Error: No matching tags found."
-        exit 1
-    else
-        echo "$latest_tag"
-    fi
+	if [ -z "$latest_tag" ]; then
+		echo "Error: No matching tags found."
+		exit 1
+	else
+		echo "$latest_tag"
+	fi
 }
-
 
 tag=$(get_latest_tag "$tag_fmt")
 pre_tag=$(get_latest_tag "$pre_tag_fmt")
 
 # Set initial tag if no tags found
 if [ -z "$tag" ]; then
-    tag=$([[ $with_v ]] && echo "v$initial_version" || echo "$initial_version")
-    if [ -z "$pre_tag" ] && $pre_release; then
-        pre_tag=$tag
-    fi
+	tag=$([[ $with_v ]] && echo "v$initial_version" || echo "$initial_version")
+	if [ -z "$pre_tag" ] && $pre_release; then
+		pre_tag=$tag
+	fi
 fi
 
 # Check for new commits
@@ -111,26 +110,26 @@ tag_commit=$(git rev-list -n 1 "$tag")
 commit=$(git rev-parse HEAD)
 
 if [ "$tag_commit" == "$commit" ]; then
-    echo "No new commits since previous tag. Skipping..."
-    setOutput "new_tag" "$tag"
-    setOutput "tag" "$tag"
-    exit 0
+	echo "No new commits since previous tag. Skipping..."
+	setOutput "new_tag" "$tag"
+	setOutput "tag" "$tag"
+	exit 0
 fi
 
 # Set default branch if necessary
 if [ -z "${default_branch}" ] && [ "$branch_history" == "full" ]; then
-    default_branch=$(git branch -rl '*/master' '*/main' | cut -d / -f2)
-    if [ -z "${default_branch}" ]; then
-        echo "::error::DEFAULT_BRANCH must not be null, something has gone wrong."
-        exit 1
-    fi
+	default_branch=$(git branch -rl '*/master' '*/main' | cut -d / -f2)
+	if [ -z "${default_branch}" ]; then
+		echo "::error::DEFAULT_BRANCH must not be null, something has gone wrong."
+		exit 1
+	fi
 fi
 
 # Get commit messages for history
 declare -A history_type=(
-    ["last"]="$(git show -s --format=%B)"
-    ["full"]="$(git log "${default_branch}"..HEAD --format=%B)"
-    ["compare"]="$(git log "${tag_commit}".."${commit}" --format=%B)"
+	["last"]="$(git show -s --format=%B)"
+	["full"]="$(git log "${default_branch}"..HEAD --format=%B)"
+	["compare"]="$(git log "${tag_commit}".."${commit}" --format=%B)"
 )
 log=${history_type[${branch_history}]}
 
@@ -138,57 +137,56 @@ printf "History:\n---\n%s\n---\n" "$log"
 
 # Determine semver bump
 determine_bump() {
-    local bump_type
+	local bump_type
 
-    case "$log" in
-    *$major_string_token*)
-        bump_type="major"
-        ;;
-    *$minor_string_token*)
-        bump_type="minor"
-        ;;
-    *$patch_string_token*)
-        bump_type="patch"
-        ;;
-    *$none_string_token*)
-        return 1
-        ;;
-    *)
-        if [ "$default_semver_bump" == "none" ]; then
-            return 1
-        else
-            bump_type="${default_semver_bump}"
-        fi
-        ;;
-    esac
+	case "$log" in
+	*$major_string_token*)
+		bump_type="major"
+		;;
+	*$minor_string_token*)
+		bump_type="minor"
+		;;
+	*$patch_string_token*)
+		bump_type="patch"
+		;;
+	*$none_string_token*)
+		return 1
+		;;
+	*)
+		if [ "$default_semver_bump" == "none" ]; then
+			return 1
+		else
+			bump_type="${default_semver_bump}"
+		fi
+		;;
+	esac
 
-    semver -i "${bump_type}" "$tag"
+	semver -i "${bump_type}" "$tag"
 }
-
 
 new_tag=$(determine_bump)
 if [ $? -eq 1 ]; then
-    echo "Default bump was set to none. Skipping..."
-    setOutput "new_tag" "$tag"
-    setOutput "tag" "$tag"
-    exit 0
+	echo "Default bump was set to none. Skipping..."
+	setOutput "new_tag" "$tag"
+	setOutput "tag" "$tag"
+	exit 0
 fi
 
 if $pre_release; then
-    if [[ $pre_tag =~ $new_tag ]] && [[ $pre_tag =~ $prerelease_suffix ]]; then
-        new_tag=$([[ $with_v ]] && echo "v$(semver -i prerelease "${pre_tag}" --preid "${prerelease_suffix}")" || echo "$(semver -i prerelease "${pre_tag}" --preid "${prerelease_suffix}")")
-        echo -e "Bumping ${prerelease_suffix} pre-tag ${pre_tag}. New pre-tag ${new_tag}"
-    else
-        new_tag=$([[ $with_v ]] && echo "v$new_tag-$prerelease_suffix.0" || echo "$new_tag-$prerelease_suffix.0")
-        echo -e "Setting ${prerelease_suffix} pre-tag ${pre_tag} - With pre-tag ${new_tag}"
-    fi
+	if [[ $pre_tag =~ $new_tag ]] && [[ $pre_tag =~ $prerelease_suffix ]]; then
+		new_tag=$([[ $with_v ]] && echo "v$(semver -i prerelease "${pre_tag}" --preid "${prerelease_suffix}")" || echo "$(semver -i prerelease "${pre_tag}" --preid "${prerelease_suffix}")")
+		echo -e "Bumping ${prerelease_suffix} pre-tag ${pre_tag}. New pre-tag ${new_tag}"
+	else
+		new_tag=$([[ $with_v ]] && echo "v$new_tag-$prerelease_suffix.0" || echo "$new_tag-$prerelease_suffix.0")
+		echo -e "Setting ${prerelease_suffix} pre-tag ${pre_tag} - With pre-tag ${new_tag}"
+	fi
 else
-    new_tag=$([[ $with_v ]] && echo "v$new_tag" || echo "$new_tag")
-    echo -e "Bumping tag ${tag} - New tag ${new_tag}"
+	new_tag=$([[ $with_v ]] && echo "v$new_tag" || echo "$new_tag")
+	echo -e "Bumping tag ${tag} - New tag ${new_tag}"
 fi
 
 if [ -n "$custom_tag" ]; then
-    new_tag="$custom_tag"
+	new_tag="$custom_tag"
 fi
 
 # Set outputs
@@ -197,7 +195,7 @@ setOutput "tag" "$new_tag"
 setOutput "old_tag" "$tag"
 
 if $dryrun; then
-    exit 0
+	exit 0
 fi
 
 # Create and push new tag
@@ -209,9 +207,9 @@ git_refs_url=$(jq .repository.git_refs_url "$GITHUB_EVENT_PATH" | tr -d '"' | se
 echo "$dt: **pushing tag $new_tag to repo $full_name"
 
 git_refs_response=$(
-curl -s -X POST "$git_refs_url"
--H "Authorization: token $GITHUB_TOKEN"
--d @- <<EOF
+	curl -s -X POST "$git_refs_url"
+	-H "Authorization: token $GITHUB_TOKEN"
+	-d @- <<EOF
 {
 "ref": "refs/tags/$new_tag",
 "sha": "$commit"
@@ -223,8 +221,8 @@ git_ref_posted=$(echo "${git_refs_response}" | jq .ref | tr -d '"')
 
 echo "::debug::${git_refs_response}"
 if [ "${git_ref_posted}" = "refs/tags/${new_tag}" ]; then
-exit 0
+	exit 0
 else
-echo "::error::Tag was not created properly."
-exit 1
+	echo "::error::Tag was not created properly."
+	exit 1
 fi
